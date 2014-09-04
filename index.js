405e76cf6d801481d764f854c60eae70cf6ac049
Edgar Hipp <hipp.edg@gmail.com>
2014-09-04 17:17:19 +0200

    update

--- index.js
+++ index.js
@@ -1,26 +1,43 @@
-module.exports = BlinkReporter;
-var Blink1 = require('node-blink1');
-var blink1 = new Blink1();
-var time=1000;
-
-blink1.red=function(callback) {
-    return blink1.fadeToRGB(time,255,0,0,callback);
-}
-
-blink1.green=function(callback) {
-    return blink1.fadeToRGB(time,0,255,0,callback);
-}
-
-blink1.yellow=function(callback) {
-    return blink1.fadeToRGB(time,255,255,0,callback);
-}
+blink1=null;
 
 function BlinkReporter(runner) {
+    try {
+       if (blink1==null){
+         var Blink1 = require('node-blink1');
+         var blink1 = new Blink1();
+       }
+       blink1.fadeToRGB(5,0,0,0);
+    } catch (e) {
+        console.log(e)
+        console.log("NO BLINKS FOUND, please plug one in !")
+        return;
+    }
+
+    var time=1000;
+    blink1.off=function(callback) {
+        return blink1.fadeToRGB(time,0,0,0,callback);
+    }
+
+    blink1.red=function(callback) {
+        return blink1.fadeToRGB(time,128,0,0,callback);
+    }
+
+    blink1.green=function(callback) {
+        return blink1.fadeToRGB(time,0,128,0,callback);
+    }
+
+    blink1.yellow=function(callback) {
+        return blink1.fadeToRGB(time,128,128,0,callback);
+    }
+
   var passes = 0;
   var failures = 0;
 
   runner.on('pass', function(test){
-      blink1.yellow();
+      if (pass % 2==1)
+          blink1.yellow();
+      else
+          blink1.off()
       passes++;
   });
 
@@ -31,9 +48,9 @@ function BlinkReporter(runner) {
   });
 
   runner.on('end', function(){
-      if (failures==0)
-          {
+      if (failures==0) {
               blink1.green();
-          }
+      }
   });
 }
+module.exports = BlinkReporter;
