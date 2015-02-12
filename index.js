fa2968c34c4c73e995c42a481182b4d82ed4603b
Edgar Hipp <hipp.edg@gmail.com>
2015-02-12 09:14:03 +0100

    syntax update

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
 blink1=null;
 var Blink1 = require('node-blink1');
+var time=1000;
 
 function BlinkReporter(runner) {
     try {
-       if (blink1==null){
+       if (blink1===null){
          blink1 = new Blink1();
        }
        blink1.fadeToRGB(5,0,0,0);
     } catch (e) {
-        console.log(e)
-        console.log("NO BLINKS FOUND, please plug one in !")
+        console.log(e);
+        console.log("NO BLINKS FOUND, please plug one in !");
         return;
     }
 
-    var time=1000;
     blink1.off=function(t) {
         if (t===undefined) t=time;
         return blink1.fadeToRGB(t,0,0,0,callback);
-    }
+    };
 
     blink1.red=function(t) {
         if (t===undefined) t=time;
         return blink1.fadeToRGB(t,128,0,0,callback);
-    }
+    };
 
     blink1.green=function(t) {
         if (t===undefined) t=time;
         return blink1.fadeToRGB(t,0,128,0,callback);
-    }
+    };
 
     blink1.yellow=function(t) {
         if (t===undefined) t=time;
         return blink1.fadeToRGB(t,128,128,0,callback);
-    }
+    };
 
   var passes = 0;
   var failures = 0;
 
   runner.on('pass', function(test){
-      if (failures==0) {
+      if (failures===0) {
         if (passes % 2==1)
             blink1.yellow(20);
         else
-            blink1.off(20)
+            blink1.off(20);
       }
       passes++;
   });
 
   runner.on('fail', function(test, err){
       blink1.red();
-      console.log('fail',test,err)
+      console.log('fail',test,err);
       failures++;
   });
 
   runner.on('end', function(){
-      if (failures==0) {
+      if (failures===0) {
           blink1.green();
       }
   });
